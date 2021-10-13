2d828a532b52c6a6771703ef8ff9f458a4063476
erneslobo <erneslobo@gmail.com>
2021-10-13 02:20:45 +0000

    initial commit

--- src/models.py
+++ src/models.py
@@ -1,33 +1,66 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+import enum
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Gender(enum.Enum):
+    male = 1
+    female = 2
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Type_Fav(enum.Enum):
+    character = 1
+    planet = 2
+    vehicle = 3
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    height = Column(Integer)
+    mass = Column(String(250))
+    hair_color = Column(String(250))
+    eye_color = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(Enum(Gender))
+    created = Column(DateTime)
+    edited = Column(DateTime)
+    url = Column(String(250))
+    homeworld = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship("Planet", backref="planet")
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    diameter = Column(Integer)
+    climate = Column(String(250))
+    gravity = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(Integer)
+    population = Column(Integer)
+    url = Column(String(250))
+    created = Column(DateTime)
+    edited = Column(DateTime)
+    url = Column(String(250))
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(Type_Fav))
 
     def to_dict(self):
         return {}
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
